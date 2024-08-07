20c705af5d6eb47a3aebbd6c71ae873a647ca6e7
KaffinPX <KaffinPX@gmail.com>
2024-08-07 03:36:27 +0300

    inscription builder over existing ScriptBuilder

---
+++ src/builder/index.ts
@@ -0,0 +1,31 @@
+import { type ScriptBuilder, addressFromScriptPublicKey, Opcodes } from "./wasm"
+
+export class Builder {
+  script: ScriptBuilder
+
+  constructor (script: ScriptBuilder) {
+    this.script = script
+  }
+
+  create (publicKey: string, inscription: string) {
+    this.script.drain()
+
+    this.script
+      .addData(publicKey)
+      .addOp(Opcodes.OpCheckSig)
+      .addOp(Opcodes.OpFalse)
+      .addOp(Opcodes.OpIf)
+      .addData(Buffer.from("kasplex"))
+      .addI64(0n)
+      .addData(Buffer.from(JSON.stringify(inscription, null, 0)))
+      .addOp(Opcodes.OpEndIf);
+  }
+
+  createCommitAddress (networkId: string) {
+    return addressFromScriptPublicKey(this.script.createPayToScriptHashScript(), networkId)!
+  }
+
+  encodeRevealSignature (signature: string) {
+    return this.script.encodePayToScriptHashSignatureScript(signature)
+  }
+}
